b1ef80e38fb797bdb6898c8abe913c098acb52c5
cappedapollo <cappedapollo@gmail.com>
2023-10-20 11:29:02 -0400

    remove .next

--- .next/static/chunks/app/error-b51db63407dedbde.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{8116:function(n,r,u){Promise.resolve().then(u.bind(u,419))},419:function(n,r,u){"use strict";function Error(){return"Error"}u.r(r),u.d(r,{default:function(){return Error}})}},function(n){n.O(0,[971,472,744],function(){return n(n.s=8116)}),_N_E=n.O()}]);
--- .next/static/chunks/app/not-found-0e4aa233e3d9f4a7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,403,668],{3912:function(e,n,s){Promise.resolve().then(s.t.bind(s,413,23)),Promise.resolve().then(s.t.bind(s,8326,23))}},function(e){e.O(0,[326,413,971,472,744],function(){return e(e.s=3912)}),_N_E=e.O()}]);
